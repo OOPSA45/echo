e5f0fb2fd4aa431bde02ceccbb00489dad9c9772
OOPSA45 <k.komv@ya.ru>
2017-05-27 16:57:41 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,18 @@
 package ru.job4j;
 
+/**
+ * Calculate.
+ *
+ * @author Kirill Komarov (k.komv@ya.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
- 
-    public static void main(String[] args) {
+    /**
+     * Main.
+     * @param args - args.
+     */
+	public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
