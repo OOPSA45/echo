8845ae6cddaf495f6d2ad229a651ffec22fa3dc0
OOPSA45 <k.komv@ya.ru>
2017-06-10 23:24:17 +0300

    task 189

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,24 @@
+package ru.job4j.max;
+
+/**
+* Package for max task.
+*
+* @author Kirill Komarov (mailto:k.komv@ya.ru)
+* @version $Id$
+* @since 0.1
+*/
+public class Max {
+
+    /** Compare.
+	* @param first первое число
+	* @param second второе число
+	* @return result
+	*/
+    public int compare(int first, int second) {
+
+        return first > second ? first : second;
+
+    }
+
+}
+
